549d197f069f816cd47f7f0ddde2ac190826c1e3
nerddotcat <gkreick@unomaha.edu>
2016-09-22 20:51:51 -0500

    Added fun message to readme.

--- readme.mk
+++ readme.mk
@@ -1,4 +1,11 @@
 
+ _   _      _ _        __        __         _     _ _ 
+| | | | ___| | | ___   \ \      / /__  _ __| | __| | |
+| |_| |/ _ \ | |/ _ \   \ \ /\ / / _ \| '__| |/ _` | |
+|  _  |  __/ | | (_) |   \ V  V / (_) | |  | | (_| |_|
+|_| |_|\___|_|_|\___/     \_/\_/ \___/|_|  |_|\__,_(_)
+
+
 I am using this repository to save solutions to programming study problems.
 
 /ctci/ - cracking the coding interview solutions
