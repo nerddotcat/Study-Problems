ea44b8e941d3086635c3ce80df7b4d97ad08b4b8
nerddotcat <gkreick@unomaha.edu>
2016-10-03 11:14:18 -0500

    Added array to BST solution.

--- ctci/c4/src/Main.java
+++ ctci/c4/src/Main.java
@@ -1,18 +1,17 @@
-
 public class Main
 {
-	
+
 	public static void main(String[] args)
 	{
 		new Main().run();
 	}
-	
+
 	public void run()
 	{
 		System.out.println("hello!");
-		
+
 		BinTree bt = new BinTree();
-		
+
 		bt.bstAdd(50);
 		bt.bstAdd(25);
 		bt.bstAdd(75);
@@ -20,9 +19,9 @@ public class Main
 		bt.bstAdd(60);
 		bt.bstAdd(110);
 		System.out.println(bt);
-		
+
 		System.out.printf("is balanced? %s\n", bt.checkBalance());
-		
+
 		Graph g = new Graph();
 		g.addDirectedEdge(1, 2);
 		g.addDirectedEdge(2, 3);
@@ -30,12 +29,35 @@ public class Main
 		g.addDirectedEdge(4, 5);
 		g.addDirectedEdge(6, 3);
 		g.addDirectedEdge(3, 1);
+
+		System.out.printf("Path from 1 to 5: %s\n", g.directedPathExists(1, 5));
+		System.out.printf("Path from 1 to 6: %s\n", g.directedPathExists(1, 6));
+		System.out.printf("Path from 6 to 1: %s\n", g.directedPathExists(6, 1));
+		System.out.printf("Path to/from 6 and 1: %s\n",
+				g.directedPathExists(1, 6) || g.directedPathExists(6, 1));
 		
-		System.out.printf("Path from 1 to 5: %s\n",g.directedPathExists(1, 5));
-		System.out.printf("Path from 1 to 6: %s\n",g.directedPathExists(1, 6));
-		System.out.printf("Path from 6 to 1: %s\n",g.directedPathExists(6, 1));
-		System.out.printf("Path to/from 6 and 1: %s\n",g.directedPathExists(1, 6)||g.directedPathExists(6, 1));
 		
+		BinTree bt2 = new BinTree();
+		int[] ar = {1,2,3,4,5,6,7,8,9,10};
+		arrayToBST(bt2,ar,0,ar.length);
+		System.out.println(bt2);
+		
+	}
+
+	/*
+	 * Given a sorted (increasingorder) array with unique integer elements,
+	 * write an algorithm to createa binary search tree with minimal height.
+	 */
+	public void arrayToBST(BinTree bt, int[] ar, int low, int high)
+	{
+		if(low >= high)
+		{
+			return;
+		}
+		int middle = (low+high)/2;
+		bt.bstAdd(ar[middle]);
+		arrayToBST(bt,ar,low,middle);
+		arrayToBST(bt,ar,middle+1,high);
 	}
 	
 }
