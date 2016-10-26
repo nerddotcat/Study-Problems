1b7163453344de6b85b57f269545ca820cd4f23e
nerddotcat <gkreick@unomaha.edu>
2016-10-26 13:58:26 -0500

    Added in place method for permutations.

--- misc/permute.py
+++ misc/permute.py
@@ -4,8 +4,23 @@
 def main():
   things = [1,2,3]
   
-  simplePermute(things)
+  #simplePermute(things)
+  swapPermute(things,len(things),0)
   
+
+def swapPermute(vals, n, i ):
+  if i == n:
+    print vals
+    return
+  
+  for x in range(i,n):
+    swap(vals,i,x)
+    swapPermute(vals,n,i+1)
+    swap(vals,x,i)
+
+
+def swap(vals, a, b):
+  vals[a],vals[b] = vals[b],vals[a]
 
 
 def simplePermute(vals,out=[]):
